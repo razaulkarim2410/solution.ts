8ef5cdebe585b56cf922d60396928fc05625b7cb
razaul karim <razaulkarim2410@gmail.com>
2025-11-18 22:48:29 +0600

    added readme.md file and problem 6 solution

--- solution.ts
+++ solution.ts
@@ -59,3 +59,22 @@ return users.filter(user => user.isActive);
 }
 
 
+interface Book {
+    title: string;
+    author: string;
+    publishedYear: number;
+    isAvailable: boolean;
+}
+function printBookDetails(book: Book): string {
+    const availability = book.isAvailable ? "Yes" : "No";
+    return `Title: ${book.title}, Author: ${book.author}, Published: ${book.publishedYear}, Available: ${availability}`;
+}
+
+const myBook: Book = {
+  title: 'The Great Gatsby',
+  author: 'F. Scott Fitzgerald',
+  publishedYear: 1925,
+  isAvailable: true,
+};
+
+printBookDetails(myBook);
